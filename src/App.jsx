79077d45a3fbccaf26bbbb3ef4bd36dddcbdb8be
BayryamB <bayryambashchoban@gmail.com>
2024-07-03 17:30:54 +0300

    basic start

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+
+//import { useState } from "react";
+function App() {
+    return (
+        <>
+            <Navigation />
+
+            <h1>Easy Travel</h1>
+
+            <p>Some text</p>
+
+            <p>Some more text</p>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
